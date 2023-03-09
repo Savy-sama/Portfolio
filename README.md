6f32670b806df98161471cee90f7ffe6e155f80d
Saurabh Biswas <visnuk252@gmail.com>
2023-03-09 19:22:44 +0530

    Final Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## What is this project?
+
+### This is my portfolio and I have attached a contact us page as well if you might need to contact with me.
---
+++ main.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/FB.jpg", width=300)
+des = """
+    Given below are the python apps I have made. Feel free to give suggestions regarding any issue 
+    by going on "Contact Me" section of the Website.
+    """
+st.info(des)
+
+
+with col2:
+    st.header("Saurabh Biswas")
+    content = """
+    Hello! My name is Saurabh. I am a full stack web developer, student at West Bengal State University, India. 
+    I am currently pursuing my Master of Science in Electronics and Tele-communication. Always in learning mode, 
+    Looking forward to explore new arenas of tech and programming.
+    """
+    st.info(content)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+dataframe = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in dataframe[:10].iterrows():
+        st.title(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"], width=300)
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in dataframe[10:].iterrows():
+        st.title(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"], width=300)
+        st.write(f"[Source Code]({row['url']})")
